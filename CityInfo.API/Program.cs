695f9769792db1058d1f32347f04212f8885d923
Ante Pasalic <anpasv6@gmail.com>
2023-10-19 16:04:49 +0200

    Add file download

--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.StaticFiles;
+
 var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,6 +13,7 @@ builder.Services.AddControllers(options =>
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<FileExtensionContentTypeProvider>();
 
 //Allow CORS
 builder.Services.AddCors(options =>
