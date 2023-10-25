2b0d595edc523275087afef4273486dcf3e45c89
Ante Pasalic <anpasv6@gmail.com>
2023-10-25 13:47:49 +0200

    Repository

---
+++ CityInfo.API/Services/CityInfoRepository.cs
@@ -0,0 +1,35 @@
+﻿using CityInfo.API.DbContexts;
+using CityInfo.API.Entities;
+
+namespace CityInfo.API.Services
+{
+    public class CityInfoRepository : ICityInfoRepository
+    {
+        private readonly CityInfoContext _context;
+
+        public CityInfoRepository(CityInfoContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+            
+        }
+        public Task<IEnumerable<City>> GetCitiesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<City?> GetCityAsync(int cityId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PointOfInterest?> GetPointOfInterestForCityAsync(int cityId, int pointOfInterestId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<PointOfInterest>> GetPointsOfInterestForCityAsync(int cityId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
